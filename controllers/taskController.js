5da924df4fa9fa6ec53d61ca2fb7e527b966df08
AlexandrZ1 <sann12346464@gmail.com>
2021-07-23 17:03:05 +0300

    Create task controllers

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,10 +1,96 @@
+let bd = require('../bd/bd')
+const fs = require('fs')
+const { v4: uuidv4 } = require('uuid')
+const moment = require('moment')
+
 class TaskController {
-  createTask(req, res) {
-    const { userId } = req.params
+  async createTask(req, res) {
+    try {
+      const { userId } = req.params
+      const { name } = req.body
+      bd.tasks = [
+        ...bd.tasks,
+        {
+          uuid: uuidv4(),
+          name: name,
+          done: false,
+          userId: userId,
+          updatedAt: moment().zone(-180).format(),
+          createdAt: moment().zone(-180).format(),
+        },
+      ]
+      const string = JSON.stringify(bd, null, '\t')
+      await fs.writeFile('./bd/bd.json', string, function (err) {
+        if (err) return console.error(err)
+        console.log('done')
+      })
+      return res.json(bd.tasks)
+    } catch (e) {
+      return res.status(500)
+    }
   }
-  updateTask() {}
-  deleteTask() {}
-  getTasks() {}
+  async updateTask(req, res) {
+    try {
+      const { userId, taskId } = req.params
+      const { name, done } = req.body
+      bd.tasks = bd.tasks.map((item) => {
+        if (item.uuid === taskId) {
+          if (!!name) item.name = name
+          if (!!done) item.done = done
+          item.updatedAt = moment().zone(-180).format()
+          return item
+        }
+        return item
+      })
+      const string = JSON.stringify(bd, null, '\t')
+      await fs.writeFile('./bd/bd.json', string, function (err) {
+        if (err) return console.error(err)
+        console.log('done')
+      })
+      return res.json(bd.tasks)
+    } catch (e) {
+      return res.status(500)
+    }
+  }
+  async deleteTask(req, res) {
+    try {
+      const { userId, taskId } = req.params
+      bd.tasks = bd.tasks.filter((item) => item.uuid !== taskId)
+      const string = JSON.stringify(bd, null, '\t')
+      await fs.writeFile('./bd/bd.json', string, function (err) {
+        if (err) return console.error(err)
+        console.log('done')
+      })
+      return res.json(bd.tasks)
+    } catch (e) {
+      return res.status(500)
+    }
+  }
+
+  async getTasks(req, res) {
+    try {
+      const { filterBy, order } = req.query
+      const { page } = req.params
+      console.log(req.query)
+      const { name, done } = req.body
+      let tasks = bd.tasks.sort(
+        order === 'asc'
+          ? (a, b) => (a.updatedAt > b.updatedAt ? 1 : -1)
+          : (a, b) => (a.updatedAt > b.updatedAt ? -1 : 1)
+      )
+      tasks = bd.tasks.filter((item) =>
+        filterBy === 'done'
+          ? item.done
+          : filterBy === 'undone'
+          ? !item.done
+          : true
+      )
+      if (!!page) return res.json(tasks.slice(5 * (page - 1), 5 * page))
+      return res.json(tasks)
+    } catch (e) {
+      return res.status(500)
+    }
+  }
 }
 
 module.exports = new TaskController()
--- routes/taskRouter.js
+++ routes/taskRouter.js
@@ -1,10 +1,10 @@
 const Router = require('express')
 const router = new Router()
-const taskController = require('')
+const taskController = require('../controllers/taskController')
 
-router.post('/:tasdId', taskController.createTask)
-router.patch('/:userId:taskId', taskController.updateTask)
-router.delete('/:userId:taskId', taskController.deleteTask)
-router.get('/:userId', taskController.getTasks)
+router.post('/:userId', taskController.createTask)
+router.patch('/:userId/:taskId', taskController.updateTask)
+router.delete('/:userId/:taskId', taskController.deleteTask)
+router.get('/:userId/:page?', taskController.getTasks)
 
 module.exports = router
